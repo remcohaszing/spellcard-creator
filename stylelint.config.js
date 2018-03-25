6c3164c65faa174d7e76eee5df4fca220bba3554
Remco Haszing <remcohaszing@gmail.com>
2018-03-25 16:50:50 +0200

    refactor: bump all the things
    Except webpack and friends.

--- stylelint.config.js
+++ stylelint.config.js
@@ -8,5 +8,6 @@ module.exports = {
     'color-named': 'never',
     'string-quotes': 'single',
     'value-keyword-case': 'lower',
+    'font-family-no-missing-generic-family-keyword': null,
   },
 };

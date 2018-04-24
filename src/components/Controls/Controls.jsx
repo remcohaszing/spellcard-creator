bd8f25067a7ca7b65a25cc0c99d1812e01a77d27
Remco Haszing <remcohaszing@gmail.com>
2018-04-24 20:17:16 +0200

    refactor: move Controls into sub module

---
+++ src/components/Controls/Controls.jsx
@@ -0,0 +1,70 @@
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import CardSelector from '../CardSelector';
+import CollapsableView from '../CollapsableView';
+import ExportButton from '../ExportButton';
+import Footer from '../Footer';
+import SourceSelector from '../SourceSelector';
+import ThemeSelector from '../ThemeSelector';
+import classes from './Controls.css';
+
+
+export default class Controls extends React.Component {
+  static propTypes = {
+    menuOpen: PropTypes.bool.isRequired,
+    onSelectionChange: PropTypes.func.isRequired,
+    onSourcesChange: PropTypes.func.isRequired,
+    onThemeChange: PropTypes.func.isRequired,
+    selection: PropTypes.shape().isRequired,
+    sources: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+    spellMap: PropTypes.shape().isRequired,
+    theme: PropTypes.shape().isRequired,
+  };
+
+  render() {
+    const {
+      menuOpen,
+      onSelectionChange,
+      onSourcesChange,
+      onThemeChange,
+      selection,
+      sources,
+      spellMap,
+      theme,
+    } = this.props;
+
+    return (
+      <div
+        className={classNames(classes.sideBar, {
+          [classes.closed]: !menuOpen,
+        })}
+      >
+        <div className={classes.controls}>
+          <CollapsableView name="Sources">
+            <SourceSelector
+              onChange={onSourcesChange}
+              sources={sources}
+            />
+          </CollapsableView>
+          <CollapsableView name="Selection">
+            <CardSelector
+              onChange={onSelectionChange}
+              selection={selection}
+              spellMap={spellMap}
+            />
+          </CollapsableView>
+          <CollapsableView name="Theme" >
+            <ThemeSelector
+              onChange={onThemeChange}
+              theme={theme}
+            />
+          </CollapsableView>
+          <ExportButton />
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}

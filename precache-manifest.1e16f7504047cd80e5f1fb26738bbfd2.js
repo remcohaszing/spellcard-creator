6fab69ce4cee4150a6040555b3171b9bc9cf5e0d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-30 20:12:10 +0000

    Deploy remcohaszing/spellcard-creator to github.com/remcohaszing/spellcard-creator.git:gh-pages

---
+++ precache-manifest.1e16f7504047cd80e5f1fb26738bbfd2.js
@@ -0,0 +1,21 @@
+self.__precacheManifest = [
+  {
+    "revision": "18152e39b28a84c5952d16fc534187c3",
+    "url": "index.html"
+  },
+  {
+    "url": "icon_512x512.3ba25194ad64932fdde1bf8e22d60855.svg"
+  },
+  {
+    "url": "icon_256x256.3ba25194ad64932fdde1bf8e22d60855.svg"
+  },
+  {
+    "url": "bcc3b568542c64868ed1.js"
+  },
+  {
+    "url": "bcc3b568542c64868ed1.css"
+  },
+  {
+    "url": "0a557763acdca6ecc73eb39ecd6586cd.json"
+  }
+];
